453d4c5530692bd62437aad3b2dc8d13d4a43e35
sheep <sheep36999@gmail.com>
2025-04-22 15:31:02 +0800

    electron-vue

---
+++ src/renderer/src/components/Versions.vue
@@ -0,0 +1,20 @@
+
+<template>
+  <a-calendar v-model="value" />
+  select: {{value}}
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const value = ref(new Date());
+  
+   
+    return {
+      value
+    }
+  },
+}
+</script>
